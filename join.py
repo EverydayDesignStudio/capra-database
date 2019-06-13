a26761f54a5e7629f7333e58f4409e27b6f95847
Jordan White <jordanrw@vt.edu>
2019-06-13 10:33:29 -0700

    Add inserting for hike and clean up code

--- join.py
+++ join.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import time
+# Script to insert all pictures from the camera db onto the projector db
+# The actual pictures will need to be transferred over as well
+
 import sqlite3
 
 DB_PROJECTOR = '/Users/Jordan/Developer/eds/capra-database/capra-projector.db'
@@ -8,28 +10,31 @@ DB_CAMERA = '/Users/Jordan/Developer/eds/capra-database/capra-camera.db'
 
 
 def main():
-    print('Starting the JOIN script')
+    print('Starting the insert script')
 
     connection_projector = sqlite3.connect(DB_PROJECTOR)
-    connection_camera = sqlite3.connect(DB_CAMERA)
-
     cursor_projector = connection_projector.cursor()
-    cursor_projector.execute("ATTACH '/Users/Jordan/Developer/eds/capra-database/capra-projector.db' AS db1")
-    cursor_projector.execute("ATTACH '/Users/Jordan/Developer/eds/capra-database/capra-camera.db' AS db2")
+    attach_statment = "ATTACH '{db}' AS camera".format(db=DB_CAMERA)
+    cursor_projector.execute(attach_statment)
 
-    # cursor_projector.execute("SELECT * FROM db1.pictures")
-    cursor_projector.execute("INSERT INTO pictures SELECT * FROM db2.pictures")
+    # Get number of pictures and hikes
+    cursor_projector.execute("SELECT COUNT(*) FROM camera.pictures")
+    all_rows = cursor_projector.fetchall()
+    num_pics = all_rows[0][0]
+
+    cursor_projector.execute("SELECT COUNT(*) FROM camera.hikes")
+    all_rows = cursor_projector.fetchall()
+    num_hikes = all_rows[0][0]
+
+    # Execute statements
+    cursor_projector.execute("INSERT INTO pictures SELECT * FROM camera.pictures")
     connection_projector.commit()
+    print('Finished inserting {n} pictures from camera'.format(n=num_pics))
 
-    # all_rows = cursor_projector.fetchall()
-    # for row in all_rows:
-    #     print(row)
+    cursor_projector.execute("INSERT INTO hikes SELECT * FROM camera.hikes")
+    connection_projector.commit()
+    print('Finished inserting {n} hikes from camera'.format(n=num_hikes))
 
-    # all_rows = cursor.fetchall()
-    # picture = self._build_picture_from_row(all_rows[0])
-    # return picture
-
-    print('Finished the command(s)')
 
 if __name__ == "__main__":
     main()

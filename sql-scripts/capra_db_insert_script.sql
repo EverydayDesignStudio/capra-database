772109c3800c019ae424f69464ac5f927db7bc8d
Jordan White <jordanrw@vt.edu>
2019-06-13 16:35:03 -0700

    Update build, transfer, and insert scripts

--- sql-scripts/capra_db_insert_script.sql
+++ sql-scripts/capra_db_insert_script.sql
@@ -1,15 +1,12 @@
-USE capra_db;
+USE capra_camera;
 
--- DELETE FROM hikes;
+-- Create a new hike
+INSERT INTO hikes (average_altitude, average_color, start_time, end_time, pictures, created_date_time, updated_date_time) VALUES
+	(0.0, 'rgb', {st}, NULL, 0, datetime(), datetime())
 
-INSERT INTO hikes ("average_altitude", "average_color", "start_time", "end_time", "pictures", "created_date_time", "update_date_time") VALUES
-	(90.82, '(161, 158, 129)', 1528704528, 1528705129, 240, datetime(), datetime()),
-	(31.21, '(161, 158, 129)', 1528701481, 1528702079, 240, datetime(), datetime()),
-	(1576.41, '(161, 158, 129)', 1530519742, 1530520343, 240, datetime(), datetime());
+-- Grab the last hike_id
+SELECT hike_id FROM hikes ORDER BY hike_id DESC LIMIT 1;
 
--- DELETE FROM pictures;
-
-INSERT INTO pictures ("time", "altitude", "color", "hike", "index_in_hike", "camera1", "camera2", "camera3", "created_date_time", "update_date_time") VALUES
-	(1528704528,90.82,'(135, 143, 138)',1,1,NULL,'/hike1/1.jpg',NULL,datetime(),datetime()),
-	(1528704531,91.21,'(128, 128, 125)',1,2,NULL,'/hike1/2.jpg',NULL,datetime(),datetime()),
-	(1528705126,46.79,'(64, 61, 56)',1,240,NULL,'/hike1/240.jpg',NULL,datetime(),datetime());
+-- Create a new picture
+INSERT INTO pictures (time, altitude, color, hike, index_in_hike, camera1, camera2, camera3, created_date_time, updated_date_time) VALUES 
+	({t}, {a}, '{c}', {h}, {i}, '{c1}', '{c2}', '{c3}', datetime(), datetime())
--- sql-scripts/capra_db_update.sql
+++
@@ -1,3 +0,0 @@
-USE capra_db;
-
-UPDATE hikes SET update_date_time=CURRENT_TIMESTAMP;

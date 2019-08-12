6d42b163a9f083682800f716236288a2604858c1
Jordan White <jordanrw@vt.edu>
2019-08-12 14:07:02 -0700

    Clean up and uncomment insert functionality

--- insert.py
+++ insert.py
@@ -5,20 +5,26 @@
 
 import sqlite3
 
-DB_PROJECTOR = '/Users/Jordan/Developer/eds/capra-database/capra-projector.db'
-DB_CAMERA = '/Users/Jordan/Developer/eds/capra-database/capra-camera.db'
+# Local
+# DB_PROJECTOR = '/Users/Jordan/Developer/eds/capra-database/capra-projector.db'
+# DB_CAMERA = '/Users/Jordan/Developer/eds/capra-database/capra-camera.db'
+
+# External
+DB_CAMERA = '/Volumes/capra-hd/capra_camera.db'
+DB_PROJECTOR = '/Volumes/capra-hd/capra_projector.db'
 
 
 def main():
     print('Starting the insert/transfer script')
 
+    # Attach the camera to the projector database
     connection_projector = sqlite3.connect(DB_PROJECTOR)
     cursor_projector = connection_projector.cursor()
-    attach_statment = "ATTACH '{db}' AS camera".format(db=DB_CAMERA)
-    cursor_projector.execute(attach_statment)
+    attach_statement = "ATTACH '{db}' AS camera".format(db=DB_CAMERA)
+    cursor_projector.execute(attach_statement)
 
     # Get number of pictures and hikes
-    cursor_projector.execute("SELECT COUNT(*) FROM camera.pictures2")
+    cursor_projector.execute("SELECT COUNT(*) FROM camera.pictures")
     all_rows = cursor_projector.fetchall()
     num_pics = all_rows[0][0]
 
@@ -26,34 +32,28 @@ def main():
     all_rows = cursor_projector.fetchall()
     num_hikes = all_rows[0][0]
 
-    # Execute statements
-    # cursor_projector.execute("INSERT INTO pictures SELECT * FROM camera.pictures2")
-    # connection_projector.commit()
-    # print('Finished inserting {n} pictures from camera'.format(n=num_pics))
-
-    # cursor_projector.execute("INSERT INTO hikes SELECT * FROM camera.hikes")
-    # connection_projector.commit()
-    # print('Finished inserting {n} hikes from camera'.format(n=num_hikes))
-
-    cursor_projector.execute("SELECT * FROM camera.pictures2")
+    # Insert Pictures
+    cursor_projector.execute("SELECT * FROM camera.pictures")
     all_rows = cursor_projector.fetchall()
+    picture_count = 0
     for row in all_rows:
-        # print(row)
-
-        # pictures2
         statement = "INSERT INTO pictures (time, altitude, color, hike, index_in_hike, camera1, camera2, camera3) VALUES ({t}, {a}, '{c}', {h}, {i}, '{c1}', '{c2}', '{c3}')".format(t=row[0], a=row[1], c=row[2], h=row[3], i=row[4], c1=row[5], c2=row[6], c3=row[7])
-
-        # pictures
-        # statement = "INSERT INTO pictures (time, altitude, color, hike, index_in_hike, camera1, camera2, camera3) VALUES ({t}, {a}, '{c}', {h}, {i}, '{c1}', '{c2}', '{c3}')".format(t=row[1], a=row[2], c=row[3], h=row[4], i=row[5], c1=row[6], c2=row[7], c3=row[8])
-
-        # print(statement)
         cursor_projector.execute(statement)
         connection_projector.commit()
+        picture_count = picture_count + 1
+    print('Finished inserting {i} of {n} pictures from camera'.format(i=picture_count, n=num_pics))
+
+    # Insert Hikes
+    cursor_projector.execute("INSERT INTO hikes SELECT * FROM camera.hikes")
+    connection_projector.commit()
+    print('Finished inserting {n} hikes from camera'.format(n=num_hikes))
 
 
-def insert_pictures():
-    print('')
+# def insert_pictures():
+#     print('')
 
+# def insert_hikes():
+#     print('')
 
 if __name__ == "__main__":
     main()
